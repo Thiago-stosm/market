090cdafd8eccc34d5868c2085dae9d0c7e801bea
Thiago <thiagosm2006@gmail.com>
2025-07-10 18:18:02 -0300

    Atributos da classe Client

--- src/model/Client.java
+++ src/model/Client.java
@@ -5,5 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Client{
-
+    private final Integer clientId;
+    private String name;
+    private String cpf;
+    private String phone;
+    private String email;
+    private String sex;
+    private String address;
+    private final LocalDate birthDate;
+    private List<Purchase> registeredPurchases;
 }
