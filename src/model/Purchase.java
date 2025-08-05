150d55e9800f4f6805f9bdc1f50f50f3d7693003
Thiago <thiagosm2006@gmail.com>
2025-08-05 10:42:14 -0300

    feat: adiciona atributos da classe Purchase

--- src/model/Purchase.java
+++ src/model/Purchase.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Purchase {
+
+    private final Integer id;
+    private final Integer client_id;
+    private final Integer client_cpf;
+
+    public Purchase(Integer id, Integer client_id, Integer client_cpf){
+        this.id = id;
+        this.client_id = client_id;
+        this.client_cpf = client_cpf;
+    }
 }
