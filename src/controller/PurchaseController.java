c4f3322da6cdf58433e682ce0d4952d7edc16d76
Thiago <thiagosm2006@gmail.com>
2025-08-09 10:18:04 -0300

    feat: adição da lógica básica para evento purchase

--- src/controller/PurchaseController.java
+++ src/controller/PurchaseController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import model.Purchase;
+
+import java.util.Map;
+import java.util.Scanner;
+
 public class PurchaseController {
+    private Purchase purchase;
+    public static void addProduct(){
+        while(true){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter the product ID: ");
+            int productID = sc.nextInt();
+
+            System.out.print("Enter the product quantity: ");
+            int productQtd = sc.nextInt();
+
+            System.out.print("Wish to register more any product? Y/N ");
+            String answer = sc.next();
+
+            if (answer.equals('N')){
+                break;
+            }
+        }
+    }
 }
--- src/model/Purchase.java
+++ src/model/Purchase.java
@@ -1,14 +1,23 @@
 package model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Purchase {
 
     private final Integer id;
     private final Integer client_id;
     private final Integer client_cpf;
+    private final BigDecimal total_price;
+    private final List<Map> prod_id_price = new ArrayList<>();
 
-    public Purchase(Integer id, Integer client_id, Integer client_cpf){
+    public Purchase(Integer id, Integer client_id, Integer client_cpf, BigDecimal total_price){
         this.id = id;
         this.client_id = client_id;
         this.client_cpf = client_cpf;
+        this.total_price = total_price;
     }
+
 }
